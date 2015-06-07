7cb6ba74bd2467a6e7873648169026f91e8f7dbc
crsgypin <crsgypin@gmail.com>
2015-06-07 22:46:52 +0800

    add new action and page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,28 @@ class EventsController < ApplicationController
 		@events = Event.all
 	end
 
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		@event.save
+
+		redirect_to :action => :index
+	end
+
+	def show
+
+	end
+
+private 
+	
+	def event_params
+		params.require(:event).permit(:name, :description)
+	end
+
+
+
+
 end
