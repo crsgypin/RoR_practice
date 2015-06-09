b1cbfdc7281fc56c9b81a383c1751100995de6dc
crsgypin <crsgypin@gmail.com>
2015-06-09 18:14:12 +0800

    Add relational db between event, attendee and category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609093548) do
+ActiveRecord::Schema.define(version: 20150609094912) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
