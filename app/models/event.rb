bb3a7a10853486a4139652c299c83b87e5ba26ad
crsgypin <crsgypin@gmail.com>
2015-06-09 19:59:45 +0800

    add group, many-to-many to event, with event_groupship

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ActiveRecord::Base
 	has_many :attendees
 	belongs_to :category
 	has_one :location
+
+	has_many :event_groupships
+	has_many :groups, :through => :event_groupships
+
 end
