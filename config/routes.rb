387eee829306b45164c128ac87016ad7ce5009c0
crsgypin <crsgypin@gmail.com>
2015-06-12 15:07:32 +0800

    Attendees: route, controller, new view, edit view, show view and index view

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   root :to => "welcome#index"
 
   # match ':controller(/:action(/:id(.:format)))', :via => :all
-  resources :events
+  resources :events do
+    resources :attendees, :controller => 'event_attendees'
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
