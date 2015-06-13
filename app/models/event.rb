c3d6be3cd6bbd027282e3f412681334ad16e484e
crsgypin <crsgypin@gmail.com>
2015-06-13 08:57:24 +0800

    location attribute, one-to-one to event, could be manipulated by event object

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,6 @@ class Event < ActiveRecord::Base
 	has_many :groups, :through => :event_groupships
 	delegate :name, :to => :category, :prefix => true, :allow_nil => true
 	
+	accepts_nested_attributes_for :location, :allow_destroy => true, :reject_if => :all_blank
+
 end
